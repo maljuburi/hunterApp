8ea0a905a775f2846afd5701c3e84d39f49c1607
Muhammad <maljuburi89@gmail.com>
2017-10-28 00:17:25 -0400

    display and positioning is fixed

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -2,20 +2,39 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 
-  $(".flash #on").on('click', function(){
-  $("#on").hide();
-  $("#off").show();
-  window.plugins.flashlight.switchOff();
+  $(".flash #on").on('click', function () {
+    $("#on").hide();
+    $("#off").show();
+    window.plugins.flashlight.switchOff();
   });
-  $(".flash #off").on('click', function(){
-  $("#on").show();
-  $("#off").hide();
-  window.plugins.flashlight.switchOn();
+  $(".flash #off").on('click', function () {
+    $("#on").show();
+    $("#off").hide();
+    window.plugins.flashlight.switchOn();
   });
-  
+
+
+  $("#camera").on('click', takephoto);
+
+  function takephoto() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI
+    });
+
+    function onSuccess(imageURI) {
+      var image = document.getElementById('myImage');
+      image.src = imageURI;
+      image.style.display = "block";
+    }
+
+    function onFail() {
+      alert("No Image was taken!");
+    }
+  }
+
 }
 
-function redirect(page){
+function redirect(page) {
   window.location.href = page;
-
 }
